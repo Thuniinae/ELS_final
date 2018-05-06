97aa8e29c6122f009291f59740b18bd12861ddd8
Ting-Shuo Hsu <tingshuo.hsu@gmail.com>
2018-05-06 16:43:55 +0800

    Re-factor codes with clang-format

--- Testbench.h
+++ Testbench.h
@@ -4,46 +4,33 @@
 #include <string>
 using namespace std;
 
-class Testbench
-{
-	public:
-		Testbench();
-		~Testbench();
+class Testbench {
+public:
+  Testbench();
+  ~Testbench();
 
-		int read_bmp(string infile_name);
-		int write_bmp(string outfile_name);
+  int read_bmp(string infile_name);
+  int write_bmp(string outfile_name);
 
-		unsigned int get_width() {
-			return width;
-		}
+  unsigned int get_width() { return width; }
 
-		unsigned int get_height() {
-			return height;
-		}
-		
-		unsigned int get_width_bytes() {
-			return width_bytes;
-		}
+  unsigned int get_height() { return height; }
 
-		unsigned int get_bytes_per_pixel() {
-			return bytes_per_pixel;
-		}
+  unsigned int get_width_bytes() { return width_bytes; }
 
-		unsigned char* get_source_image () {
-			return source_bitmap;
-		}
-		unsigned char* get_target_image () {
-			return target_bitmap;
-		}
-    
-	private:
-		unsigned int rgb_raw_data_offset;
-		unsigned int width;
-		unsigned int height;
-		unsigned int width_bytes;
-		unsigned char  bits_per_pixel;
-		unsigned short bytes_per_pixel;
-		unsigned char *source_bitmap;
-		unsigned char *target_bitmap;
+  unsigned int get_bytes_per_pixel() { return bytes_per_pixel; }
+
+  unsigned char *get_source_image() { return source_bitmap; }
+  unsigned char *get_target_image() { return target_bitmap; }
+
+private:
+  unsigned int rgb_raw_data_offset;
+  unsigned int width;
+  unsigned int height;
+  unsigned int width_bytes;
+  unsigned char bits_per_pixel;
+  unsigned short bytes_per_pixel;
+  unsigned char *source_bitmap;
+  unsigned char *target_bitmap;
 };
 #endif
--- main.cpp
+++ main.cpp
@@ -5,42 +5,40 @@ using namespace std;
 // Wall Clock Time Measurement
 #include <sys/time.h>
 
-#include "Testbench.h"
 #include "Sobel.h"
-
+#include "Testbench.h"
 
 // TIMEVAL STRUCT IS Defined ctime
-// use start_time and end_time variables to capture 
+// use start_time and end_time variables to capture
 // start of simulation and end of simulation
 struct timeval start_time, end_time;
 
-//int main(int argc, char *argv[])
-int sc_main(int argc, char **argv)
-{
-    if((argc < 3)||(argc > 4))
-    {
-      cout << "No arguments for the executable : "<< argv[0] << endl;
-      cout << "Usage : >" << argv[0] << " in_image_file_name out_image_file_name" << endl ;
-      return 0;
-    }
-    cout << "Starting Simulation" << endl;
-
-		Testbench tb;
-		tb.read_bmp(argv[1]);
-
-    gettimeofday(&start_time, 0);
-		// Processing
-		sobel(90.0, tb.get_width(), tb.get_height(), tb.get_bytes_per_pixel(), tb.get_source_image(), tb.get_target_image());
-    gettimeofday(&end_time, 0);
-		
-		tb.write_bmp(argv[2]);
-
-    int total_usecs = (end_time.tv_sec - start_time.tv_sec) * 1000000 +
-                     (end_time.tv_usec - start_time.tv_usec);
-
-    cout << "==============================================="<< endl;
-    cout << "   Time Taken for simulation is: " << total_usecs << " uS" << endl; 
-    cout << "==============================================="<< endl;
+// int main(int argc, char *argv[])
+int sc_main(int argc, char **argv) {
+  if ((argc < 3) || (argc > 4)) {
+    cout << "No arguments for the executable : " << argv[0] << endl;
+    cout << "Usage : >" << argv[0] << " in_image_file_name out_image_file_name"
+         << endl;
     return 0;
-}
+  }
+  cout << "Starting Simulation" << endl;
 
+  Testbench tb;
+  tb.read_bmp(argv[1]);
+
+  gettimeofday(&start_time, 0);
+  // Processing
+  sobel(90.0, tb.get_width(), tb.get_height(), tb.get_bytes_per_pixel(),
+        tb.get_source_image(), tb.get_target_image());
+  gettimeofday(&end_time, 0);
+
+  tb.write_bmp(argv[2]);
+
+  int total_usecs = (end_time.tv_sec - start_time.tv_sec) * 1000000 +
+                    (end_time.tv_usec - start_time.tv_usec);
+
+  cout << "===============================================" << endl;
+  cout << "   Time Taken for simulation is: " << total_usecs << " uS" << endl;
+  cout << "===============================================" << endl;
+  return 0;
+}

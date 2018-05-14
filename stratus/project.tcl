41d78d1053d6a02608a68885e75a56f7371d24af
Ting-Shuo Hsu <tingshuo.hsu@gmail.com>
2018-05-14 10:01:43 +0000

    Set clock period as 10 ns and add (back) dpopt

--- stratus/project.tcl
+++ stratus/project.tcl
@@ -19,7 +19,7 @@ use_tech_lib    "$LIB_PATH/$LIB_LEAF"
 #
 # Global synthesis attributes.
 #
-set_attr clock_period           1.0
+set_attr clock_period           10.0
 set_attr message_detail         3 
 #set_attr default_input_delay    0.1
 #set_attr sched_aggressive_1 on
@@ -31,7 +31,7 @@ set_attr message_detail         3
 #
 # Simulation Options
 #
-set_attr cc_options             "-DCLOCK_PERIOD=1.0 -g"
+set_attr cc_options             "-DCLOCK_PERIOD=10.0 -g"
 #enable_waveform_logging -vcd
 set_attr end_of_sim_command "make saySimPassed"
 
@@ -48,14 +48,13 @@ define_system_module ../System.cpp
 define_hls_module SobelFilter ../SobelFilter.cpp
 
 define_hls_config SobelFilter BASIC
-#define_hls_config SobelFilter DPA       --dpopt_auto=op,expr
+define_hls_config SobelFilter DPA       --dpopt_auto=op,expr
 
 set IMAGE_DIR           ".."
 set IN_FILE_NAME        "${IMAGE_DIR}/lena_std_short.bmp"
 set OUT_FILE_NAME				"out.bmp"
 
 define_sim_config B -argv "$IN_FILE_NAME $OUT_FILE_NAME"
-#foreach cfg { BASIC DPA } {
-foreach cfg { BASIC } {
+foreach cfg { BASIC DPA } {
 	define_sim_config V_${cfg} "SobelFilter RTL_V ${cfg}" -argv "$IN_FILE_NAME $OUT_FILE_NAME"
 }

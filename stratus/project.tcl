6a977f09721279a5b0bde931313b2c8231715efb
Ting-Shuo Hsu <tingshuo.hsu@gmail.com>
2018-10-23 13:59:41 +0800

    Simplify and add comments in project.tcl

--- stratus/project.tcl
+++ stratus/project.tcl
@@ -31,6 +31,7 @@ set_attr message_detail         3
 #
 # Simulation Options
 #
+### 1. You may add your own options for C++ compilation here.
 set_attr cc_options             "-DCLOCK_PERIOD=10.0 -g"
 #enable_waveform_logging -vcd
 set_attr end_of_sim_command "make saySimPassed"
@@ -38,6 +39,7 @@ set_attr end_of_sim_command "make saySimPassed"
 #
 # Testbench or System Level Modules
 #
+### 2. Add your testbench source files here.
 define_system_module ../main.cpp
 define_system_module ../Testbench.cpp
 define_system_module ../System.cpp
@@ -45,8 +47,10 @@ define_system_module ../System.cpp
 #
 # SC_MODULEs to be synthesized
 #
+### 3. Add your design source files here (to be high-level synthesized).
 define_hls_module SobelFilter ../SobelFilter.cpp
 
+### 4. Define your HLS configuration (arbitrary names, BASIC and DPA in this example). 
 define_hls_config SobelFilter BASIC
 define_hls_config SobelFilter DPA       --dpopt_auto=op,expr
 
@@ -54,7 +58,10 @@ set IMAGE_DIR           ".."
 set IN_FILE_NAME        "${IMAGE_DIR}/lena_std_short.bmp"
 set OUT_FILE_NAME				"out.bmp"
 
+### 5. Define simulation configuration for each HLS configuration
+### 5.1 The behavioral simulation (C++ only).
 define_sim_config B -argv "$IN_FILE_NAME $OUT_FILE_NAME"
-foreach cfg { BASIC DPA } {
-	define_sim_config V_${cfg} "SobelFilter RTL_V ${cfg}" -argv "$IN_FILE_NAME $OUT_FILE_NAME"
-}
+### 5.2 The Verilog simulation for HLS config "BASIC". 
+define_sim_config V_BASIC "SobelFilter RTL_V BASIC" -argv "$IN_FILE_NAME $OUT_FILE_NAME"
+### 5.3 The Verilog simulation for HLS config "DPA". 
+define_sim_config V_DPA "SobelFilter RTL_V DPA" -argv "$IN_FILE_NAME $OUT_FILE_NAME"

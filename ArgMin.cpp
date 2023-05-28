758ad46ec8517c2339c05d3ae90cfadfa1ea7b92
Thuniinae <thunia4182@gmail.com>
2023-05-28 22:23:17 +0800

    [update] parallel minimum AM

--- ArgMin.cpp
+++ ArgMin.cpp
@@ -24,20 +24,61 @@ void ArgMin::do_calculation(){
 	}
     while (true){
 		sc_biguint<144> distance;
-		sc_uint<16> min;
-		sc_uint<3> index;
+		sc_uint<16> t1, t2, t3, t4;
+		sc_uint<3> i1, i2, i3, i4;
 
 		distance = read();
-		min = distance.range(17, 0);
-		index = 0;
-		// can be more efficient
-		for (int i = 1; i < 8; i++) {
-			if (distance.range((i * 18) + 17, (i * 18)) < min) {
-				min = distance.range((i * 18) + 17, (i * 18));
-				index = i;
+		{	// parallel find minimum
+			#ifndef NATIVE_SYSTEMC
+				HLS_CONSTRAIN_LATENCY(0, HLS_ACHIEVABLE, "AG_loop");
+			#endif
+			// stage 1
+			if (distance.range(17, 0) <= distance.range(35, 18)){
+				t1 = distance.range(17, 0);
+				i1 = 0;
 			}
+			else{
+				t1 = distance.range(35, 18);
+				i1 = 1;
+			}
+			if (distance.range(53, 36) <= distance.range(71, 54)){
+				t2 = distance.range(53, 36);
+				i2 = 2;
+			}
+			else {
+				t2 = distance.range(71, 54);
+				i2 = 3;
+			}
+			if (distance.range(89, 72) <= distance.range(107,90)){
+				t3 = distance.range(89, 72);
+				i3 = 4;
+			}
+			else {
+				t3 = distance.range(107, 90);
+				i3 = 5;
+			}
+			if (distance.range(125, 108) <= distance.range(143,126)){
+				t4 = distance.range(125, 108);
+				i4 = 6;
+			}
+			else {
+				t4 = distance.range(143,126);
+				i4 = 7;
+			}
+			// stage 2
+			if (t1 <= t2){
+				t2 = t1;
+				i2 = i1;
+			}
+			if (t3 <= t4){
+				t4 = t3;
+				i4 = i3;
+			}
+			// stage 3
+			if (t2 <= t4)
+				i4 = i2;
 		}
-		write(index);
+		write(i4);
 	}
 }
 
